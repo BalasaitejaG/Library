0d9ba564166194ca3efe6b11d854871ac9773aea
Balasaiteja1 <balasaiteja17@gmail.com>
2024-03-18 12:31:04 +0530

    reset to start

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -113,6 +113,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'library/media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS =os.path.join(BASE_DIR, "library/static")
-STATIC_ROOT= os.path.join(BASE_DIR, "staticfiles_build","library/static")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
